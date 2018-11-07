ecc08bce277b802a31ac2e1da538d0ecc69a5d54
aguiot-- <aguiot--@student.42.fr>
2018-11-07 14:50:46 +0100

    ft_strlen

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/11/07 11:37:02 by aguiot--          #+#    #+#              #
-#    Updated: 2018/11/07 14:31:00 by aguiot--         ###   ########.fr        #
+#    Updated: 2018/11/07 14:42:51 by aguiot--         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ NAME = libft.a
 INCLUDES = ./
 CFLAGS += -Wall -Wextra -Werror
 SRC = ft_putchar_fd.c ft_putchar.c ft_putstr_fd.c ft_putstr.c ft_putnbr_fd.c \
-	  ft_putnbr.c
+	  ft_putnbr.c ft_strlen.c
 OBJ = $(SRC:.c=.o)
 
 all: $(NAME)
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,24 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/07 11:36:31 by aguiot--          #+#    #+#             */
-/*   Updated: 2018/11/07 14:49:02 by aguiot--         ###   ########.fr       */
+/*   Created: 2018/11/07 14:44:16 by aguiot--          #+#    #+#             */
+/*   Updated: 2018/11/07 14:49:13 by aguiot--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+int		ft_strlen(char const *s)
+{
+	int	i;
 
-void	ft_putchar_fd(char c, int fd);
-void	ft_putchar(char c);
-void	ft_putstr_fd(char const *s, int fd);
-void	ft_putstr(char const *s);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putnbr(int n);
-int		ft_strlen(char const *s);
-
-#endif
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}

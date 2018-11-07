1a27e99a519f1da5068648b571e957823082d544
aguiot-- <aguiot--@student.42.fr>
2018-11-07 12:49:43 +0100

    ft_putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,20 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/07 11:36:31 by aguiot--          #+#    #+#             */
-/*   Updated: 2018/11/07 12:54:36 by aguiot--         ###   ########.fr       */
+/*   Created: 2018/11/07 12:47:06 by aguiot--          #+#    #+#             */
+/*   Updated: 2018/11/10 09:53:06 by aguiot--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-void ft_putchar_fd(char c, int fd);
-void ft_putchar(char c);
-void ft_putstr_fd(char const *s, int fd);
-
-#endif
+void	ft_putstr_fd(char const *s, int fd)
+{
+	while (s && *s)
+		ft_putchar_fd(*s++, fd);
+}

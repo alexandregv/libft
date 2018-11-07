2da09fedba37821e192f904aa099a01fa9fc95ec
aguiot-- <aguiot--@student.42.fr>
2018-11-07 17:45:17 +0100

    ft_strncpy

--- libft.h
+++ libft.h
@@ -6,12 +6,14 @@
 /*   By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 11:36:31 by aguiot--          #+#    #+#             */
-/*   Updated: 2018/11/07 16:04:03 by aguiot--         ###   ########.fr       */
+/*   Updated: 2018/11/07 17:07:07 by aguiot--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# include <string.h>
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putchar(char c);
@@ -21,6 +23,7 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_putnbr(int n);
 int		ft_strlen(char const *s);
 char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strdup(const char *s1);
 
 #endif

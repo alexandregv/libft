cfb30be63f40d5cccfe9e8ceb2ef65b67c0d0bd7
aguiot-- <aguiot--@student.42.fr>
2018-11-08 18:01:11 +0100

    ft_islower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 11:36:31 by aguiot--          #+#    #+#             */
-/*   Updated: 2018/11/08 17:49:38 by aguiot--         ###   ########.fr       */
+/*   Updated: 2018/11/08 18:00:02 by aguiot--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ char	*ft_strrchr(char const *s, int c);
 int		ft_strcmp(char const *s1, char const *s2);
 int		ft_strncmp(char const *s1, char const *s2, size_t n);
 int		ft_isupper(int c);
+int		ft_islower(int c);
 
 #endif

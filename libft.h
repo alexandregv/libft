f2b9ae8a2417047276f338b01e85709e77122b29
aguiot-- <aguiot--@student.42.fr>
2018-11-12 12:40:18 +0100

    ft_isxdigit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 11:36:31 by aguiot--          #+#    #+#             */
-/*   Updated: 2018/11/12 12:24:43 by aguiot--         ###   ########.fr       */
+/*   Updated: 2018/11/12 12:40:07 by aguiot--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,6 @@ int		ft_isspace(int c);
 int		ft_isblank(int c);
 int		ft_iscntrl(int c);
 int		ft_isgraph(int c);
+int		ft_isxdigit(int c);
 
 #endif

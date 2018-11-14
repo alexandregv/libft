66f4dda39a62fa7c8a7fdaf80a7ca0d7dfcfb81d
aguiot-- <aguiot--@student.42.fr>
2018-11-14 13:09:45 +0100

    ft_str_skip_while

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aguiot-- <aguiot--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 11:36:31 by aguiot--          #+#    #+#             */
-/*   Updated: 2018/11/14 12:59:05 by aguiot--         ###   ########.fr       */
+/*   Updated: 2018/11/14 13:06:53 by aguiot--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,9 +94,10 @@ char				*ft_itoa(int n);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
-void				ft_lstdel(t_list **alst, void(*del)(void *, size_t));
-void				ft_lstiter(t_list *lst, void(*f)(t_list *elem));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 int					ft_str_skip_to(char *str, char const c);
+int					ft_str_skip_while(char *str, int (*f)(int));
 
 #endif
